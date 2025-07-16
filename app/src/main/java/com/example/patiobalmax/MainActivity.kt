b596a13a6cec1527f9caea23f014216b2bd5a1b1
balmaxsoporte <balmaxsoporte@gmail.com>
2025-07-16 08:11:44 -0400

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/patiobalmax/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.patiobalmax
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Esta clase es opcional y puede ser eliminada si no se usa.
+        // Se incluye por compatibilidad con ciertos flujos de creación de proyectos.
+    }
+}
